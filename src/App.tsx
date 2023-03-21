e3dfdd816fe8da83832ca116d007c33cbcbb831b
kartik <codebull707@gmail.com>
2023-03-21 23:35:23 +0530

    feat(UI): use Chakra UI! ⚫

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
-
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
+const App = () => {
+  const { toggleColorMode } = useColorMode();
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
+    <Container maxW="container.xl" h="100vh">
+      <Center p="10px">
+        <Box me="20px">
+          <Text fontSize="30px">Navbar</Text>
+        </Box>
+        <Button onClick={toggleColorMode}>Toggle darkness!</Button>
+      </Center>
+      This is an app with dark theme toggle
+    </Container>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
 import App from "./App";
+
 import "./styles.css";
+import theme from "./config/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );

ff7a88ba904479924cb7e20d04125dcfbf9572de
kartik <codebull707@gmail.com>
2023-05-18 00:43:50 +0530

    refactor: provide theme object to chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import theme from './config/theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>

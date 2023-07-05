3afb264311817134455df5a01cd9745ccddb2a4e
kartik <codebull707@gmail.com>
2023-07-06 00:25:52 +0530

    test: test logic for navbar 🧭2

--- src/tests/layout.test.tsx
+++ src/tests/layout.test.tsx
@@ -2,19 +2,21 @@ import { beforeEach, describe, it, vi } from 'vitest'
 import { render, screen } from '~/utils/test-utils'
 import Root from '~/Root'
 import { useCustomTheme } from '~/hooks/useCustomTheme/useCustomTheme'
+import { fireEvent } from '@testing-library/react'
+import { ThemeName } from '~/config/allThemes'
 
 vi.mock('~/hooks/useCustomTheme/useCustomTheme')
 
 describe('Describe the Layout', () => {
-  let themeChanged: boolean = false
+  let themeName: string = ''
 
   beforeEach((context) => {
     useCustomTheme.mockReturnValue({
       theme: {
         name: 'any_theme',
       },
-      setThemeName: () => {
-        themeChanged = true
+      setThemeName: (_themeName: string) => {
+        themeName = _themeName
       },
     })
     context.component = render(<Root />)
@@ -23,9 +25,20 @@ describe('Describe the Layout', () => {
   it('should have a navbar', async ({ expect, component }) => {
     const navbar = component.getByTestId('navbar')
     expect(navbar).toBeDefined()
+    // Testing the content of the navbar
     expect(await screen.findByText('Menu')).toBeDefined()
     expect(await screen.findByText('any_theme')).toBeDefined()
     expect(await screen.findByText('Green')).toBeDefined()
+
+    // Testing the theme change dropdown menu
+    fireEvent.click(screen.getByText('any_theme'))
+    expect(await screen.findByText(ThemeName.Default)).toBeDefined()
+
+    // Testing the theme change click event
+    fireEvent.click(screen.getByText(ThemeName.Default))
+    expect(themeName).toBe(ThemeName.Default)
+
+    // TODO: simulate the state change event
   })
 
   it('should have a sidebar', ({ expect, component }) => {

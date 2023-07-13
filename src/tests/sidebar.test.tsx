1f2ce2c8cba04407fef5edfe77eeb5b7f1fd6b55
kartik <codebull707@gmail.com>
2023-07-13 12:22:52 +0530

    test: add separate tests for sidebar 🧪

---
+++ src/tests/sidebar.test.tsx
@@ -0,0 +1,13 @@
+import { beforeEach, describe, it } from 'vitest'
+import { render, screen } from '~/utils/test-utils'
+import Sidebar from '~/components/Sidebar/Sidebar'
+
+describe('Test the Sidebar', () => {
+  beforeEach(() => {
+    render(<Sidebar />)
+  })
+
+  it.skip('should have a sidebar', ({ expect }) => {
+    expect(screen.getByText('Sidebar')).toBeTruthy()
+  })
+})

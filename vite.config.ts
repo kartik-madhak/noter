b1f60e89683e051b854bcd8e6a213660fb81330f
kartik-madhak <58913047+kartik-madhak@users.noreply.github.com>
2023-08-18 02:28:43 +0530

    fix: exclude e2e from vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import { configDefaults } from 'vitest/config'
 
 const mobile =
   process.env.TAURI_PLATFORM === 'android' ||
@@ -35,5 +36,6 @@ export default defineConfig(async () => ({
   },
   test: {
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'src/e2e/*'],
   },
 }))

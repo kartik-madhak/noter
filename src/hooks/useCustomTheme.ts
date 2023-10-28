68040ccb37e7471ab2254f77fee5ae86cc8b8e7b
kartik-madhak <58913047+kartik-madhak@users.noreply.github.com>
2023-10-28 22:00:27 +0530

    feat: Can change the editor theme independently now! 🌈

--- src/hooks/useCustomTheme.ts
+++ src/hooks/useCustomTheme.ts
@@ -1,17 +1,29 @@
 import { useColorMode } from '@chakra-ui/react'
 import useLocalStorageState from 'use-local-storage-state'
 import { useEffect } from 'react'
+import * as theMirrorThemes from 'thememirror'
 import allThemes, { type Theme, ThemeName, ThemeType } from '~/config/allThemes'
 
 export const useCustomTheme = (): {
   theme: Theme
   setThemeName: (themeName: ThemeName) => void
+  editorTheme: any
+  editorThemeName: string
+  setEditorThemeName: (editorThemeName: string) => void
 } => {
   const [themeName, setThemeName] = useLocalStorageState<ThemeName>('theme', {
     defaultValue: ThemeName.Default,
   })
+  const [editorThemeName, setEditorTheme] = useLocalStorageState<string>(
+    'editorTheme',
+    {
+      defaultValue: 'barf',
+    }
+  )
   const { colorMode, toggleColorMode } = useColorMode()
   const theme = allThemes[themeName]
+  const editorTheme =
+    theMirrorThemes[editorThemeName as keyof typeof theMirrorThemes]
 
   useEffect(() => {
     if (
@@ -22,5 +34,11 @@ export const useCustomTheme = (): {
     }
   }, [theme.type, colorMode])
 
-  return { theme, setThemeName }
+  return {
+    theme,
+    setThemeName,
+    editorTheme,
+    editorThemeName,
+    setEditorThemeName: setEditorTheme,
+  }
 }

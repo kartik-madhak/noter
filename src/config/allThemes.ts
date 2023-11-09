0d8f163df65ec1bfc4b66d0b2cc4749d3fc8f7d4
kartik-madhak <58913047+kartik-madhak@users.noreply.github.com>
2023-11-10 00:13:31 +0530

    refactor: remove unused variable editorColor

--- src/config/allThemes.ts
+++ src/config/allThemes.ts
@@ -13,7 +13,6 @@ export interface Theme {
   type: ThemeType
   sidebarColor: string
   navbarColor: string
-  editorColor: string
 }
 
 const allThemes: {
@@ -24,7 +23,6 @@ const allThemes: {
     type: ThemeType.Dark,
     sidebarColor: '#212121',
     navbarColor: '#303030',
-    editorColor: '#292929',
   },
   [ThemeName.Lofi]: {
     name: ThemeName.Lofi,
@@ -33,8 +31,6 @@ const allThemes: {
       'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)),linear-gradient(179.52deg, rgb(164, 192, 247) 7.08%, rgb(169, 228, 232)34.94%, rgb(176, 226, 184) 65.12%, rgb(207, 223, 162) 96.23%)',
     navbarColor:
       'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)),linear-gradient(179.52deg, rgb(164, 192, 247) 7.08%, rgb(169, 228, 232)34.94%, rgb(176, 226, 184) 65.12%, rgb(207, 223, 162) 96.23%)',
-    editorColor:
-      'linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), linear-gradient(179.52deg, rgb(164, 192, 247) 7.08%, rgb(169, 228, 232) 34.94%, rgb(176, 226, 184) 65.12%, rgb(207, 223, 162) 96.23%)',
   },
 }
 

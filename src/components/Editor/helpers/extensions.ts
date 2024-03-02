8fba693b1fb3d22471a62f094d3a4ba9b0d159ca
kartik-madhak <58913047+kartik-madhak@users.noreply.github.com>
2024-03-02 22:59:01 +0530

    refactor: refactor sub-methods out of main method

--- src/components/Editor/helpers/extensions.ts
+++ src/components/Editor/helpers/extensions.ts
@@ -5,21 +5,27 @@ import {
 } from '@codemirror/state'
 import { invoke } from '@tauri-apps/api/tauri'
 
+const saveMetadata = (tr: Transaction, openedFile: string): void => {
+  const cursorPosition = tr?.state?.selection?.main?.head
+  if (cursorPosition != null) {
+    void invoke('save_metadata', {
+      path: openedFile,
+      cursor: cursorPosition,
+    })
+  }
+}
+
+const saveFile = (tr: Transaction, openedFile: string): void => {
+  if (tr.docChanged) {
+    const content = tr.newDoc.toString()
+    void invoke('save_file', { path: openedFile, content })
+  }
+}
+
 export const autoSave = (openedFile: string): Extension => {
   return EditorState.transactionExtender.of((tr: Transaction) => {
-    if (tr.docChanged) {
-      const content = tr.newDoc.toString()
-      void invoke('save_file', { path: openedFile, content })
-    }
-
-    const cursorPosition = tr?.state?.selection?.main?.head
-    if (cursorPosition != null) {
-      console.log('saving metadata')
-      void invoke('save_metadata', {
-        path: openedFile,
-        cursor: cursorPosition,
-      })
-    }
+    saveFile(tr, openedFile)
+    saveMetadata(tr, openedFile)
     return null
   })
 }
